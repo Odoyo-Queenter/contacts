1491768aac9eaed7f61f8a53011c8e7ed471483f
Odoyo-Queenter <achiengodoyoqueenter@gmail.com>
2022-07-23 21:47:57 +0300

    images

--- app/src/main/java/dev/queenter/mycontacts/ContactsRvAdapter.kt
+++ app/src/main/java/dev/queenter/mycontacts/ContactsRvAdapter.kt
@@ -7,43 +7,46 @@ import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import dev.queenter.mycontacts.databinding.ContactListItemBinding
 
-class ContactsRvAdapter (var contactList: List<Contact>):RecyclerView.Adapter<ContactsRvAdapter.ContactsViewHolder>(){
+class ContactsRvAdapter (var contactList: List<Contact>):
+    RecyclerView.Adapter<ContactsViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsViewHolder {
-        var binding=ContactListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-        var contactsViewHolder=ContactsViewHolder(binding)
+        var binding = ContactListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        var contactsViewHolder = ContactsViewHolder(binding)
         return contactsViewHolder
     }
 
     override fun onBindViewHolder(holder: ContactsViewHolder, position: Int) {
-        var currentContact=contactList.get(position)
-        holder.binding.tvName.text=currentContact.name
-        holder.binding.tvPhoneNumber.text=currentContact.PhoneNumber
-        holder.binding.tvEmail.text=currentContact.email
-        holder.binding.tvAddress.text=currentContact.address
+        var currentContact = contactList.get(position)
+        holder.binding.tvName.text = currentContact.name
+        holder.binding.tvPhoneNumber.text = currentContact.PhoneNumber
+        holder.binding.tvEmail.text = currentContact.email
+        holder.binding.tvAddress.text = currentContact.address
         Picasso.get()
             .load(currentContact.image)
             .placeholder(R.drawable.ic_baseline_person_pin_24)
             .error(R.drawable.ic_baseline_error_24)
-            .resize(350,350) //
+            .resize(350, 350)
             .into(holder.binding.imgContactimage)
-        val context=holder.itemView.context
+        val context = holder.itemView.context
         holder.binding.cvContact.setOnClickListener {
-            val intent=Intent(context,viewContactActivity::class.java)
-            intent.putExtra("NAME",currentContact.name)
-            intent.putExtra("EMAIL",currentContact.email)
-            intent.putExtra("PHONENUMBER",currentContact.PhoneNumber)
-            intent.putExtra("ADDRESS",currentContact.address)
-            intent.putExtra("IMAGE",currentContact.image)
+            val intent = Intent(context, viewContactActivity::class.java)
+            intent.putExtra("NAME", currentContact.name)
+            intent.putExtra("EMAIL", currentContact.email)
+            intent.putExtra("PHONENUMBER", currentContact.PhoneNumber)
+            intent.putExtra("ADDRESS", currentContact.address)
+            intent.putExtra("IMAGE", currentContact.image)
             context.startActivity(intent)
 
         }
-
-    override fun getItemCount(): Int {
-        return  contactList.size
     }
+
+        override fun getItemCount(): Int {
+            return contactList.size
+        }
+
 }
 class ContactsViewHolder(var binding:ContactListItemBinding):
     RecyclerView.ViewHolder(binding.root){
 
 
-}    }
+}
--- app/src/main/java/dev/queenter/mycontacts/viewContactActivity.kt
+++ app/src/main/java/dev/queenter/mycontacts/viewContactActivity.kt
@@ -2,10 +2,45 @@ package dev.queenter.mycontacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.squareup.picasso.Picasso
+import dev.queenter.mycontacts.databinding.ActivityViewContactBinding
 
 class viewContactActivity : AppCompatActivity() {
+    lateinit var binding: ActivityViewContactBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_contact)
+//        setContentView(R.layout.activity_view_contact)
+        binding = ActivityViewContactBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        getExtras()
     }
+    fun getExtras(){
+        val extras=intent.extras  //IS A NULL value
+        val name=extras?.getString("NAME","")
+        val email=extras?.getString("EMAIL","")
+        val phone=extras?.getString("PHONENUMBER","")
+
+        val image=binding.imgViewing
+        val addressing=extras?.getString("ADDRESS","")
+
+
+        Toast.makeText(this,"$name: $email",Toast.LENGTH_LONG).show()
+        Toast.makeText(this,name,Toast.LENGTH_LONG).show()
+        Toast.makeText(this,phone,Toast.LENGTH_LONG).show()
+        Toast.makeText(this,addressing,Toast.LENGTH_LONG).show()
+
+
+
+        binding.tvName2.text=name
+        binding.tvPhoneNumber2.text=addressing
+        binding.tvSimCard.text=phone
+
+        Picasso.get().load(intent.getStringExtra("IMAGE")).into(image)
+
+
+    }
+
+
+
 }
